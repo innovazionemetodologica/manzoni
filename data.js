215962d8b38440d50e6569d3b41bee75a45634ca
innovazionemetodologica <75805544+innovazionemetodologica@users.noreply.github.com>
2020-12-10 19:20:33 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,48 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-download-1",
+      "name": "download (1)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 79.5,
+      "initialViewParameters": {
+        "yaw": 0.26532310578332385,
+        "pitch": -0.04318939075266037,
+        "fov": 1.2061874717363192
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3183177259952803,
+          "pitch": 0.10963657649924485,
+          "rotation": 0,
+          "target": "0-download-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.38022490768537054,
+          "pitch": -0.08960599531443236,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">LIM</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm</font></font>"
+        }
+      ]
+    }
+  ],
+  "name": "prova",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
